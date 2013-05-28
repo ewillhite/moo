503af57221a79d919ece8f72320cbbf2476dcffd
Evan Willhite <ewillhite@centresource.com>
2013-05-27 21:13:09 -0500

    added mysql file, nodequeue_extras, favicon and wrapped up general styling

--- sites/all/themes/moo/templates/page.tpl.php
+++ sites/all/themes/moo/templates/page.tpl.php
@@ -27,16 +27,6 @@
   <div class="page">
 
     <div role="main" id="main-content">
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-
       <?php if (isset($tabs['#primary'][0]) || isset($tabs['#secondary'][0])): ?>
         <nav class="tabs"><?php print render($tabs); ?></nav>
       <?php endif; ?>
@@ -47,6 +37,15 @@
         <?php print $messages; ?>
         </div>
       <?php } ?>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
 
 
       <?php print render($page['content_pre']); ?>
