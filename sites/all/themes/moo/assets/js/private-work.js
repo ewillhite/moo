26a97aeda62341740c20ef67b50964d3a1151413
root <root@12southdev.com>
2013-07-08 09:40:16 -0400

    added Drupal.behaviors

--- sites/all/themes/moo/assets/js/private-work.js
+++ sites/all/themes/moo/assets/js/private-work.js
@@ -1,21 +1,26 @@
 (function ($) {
 
-$(function() {
-
-	// Date Search
+Drupal.behaviors.mymoo = {
+  attach: function(context) {
+  
+  	// Date Search
 	label = $('.view-private-work .views-exposed-widget > label');
-	$('<span class="arrow"></span>').insertAfter(label);
-	$(label).addClass('clickable').click(function() {
+	if (!$('.arrow').length) {
+		$('<span class="arrow"></span>').insertAfter(label);	
+	}
+	$(label).addClass('clickable').once().click(function() {
 		widget = $(this).siblings('.views-widget');
 		$('.view-private-work .views-widget').not(widget).hide();
 		$(widget).toggle();
 	});
-	
+  
 	$('.vbo-views-form legend a').click();
 	
 	$('.vbo-select').click(function () {
-	    $('.vbo-views-form legend a').click();
+		$('.vbo-views-form legend a').click();
 	});
-});
+  
+  }
+};
 
 })(jQuery);
