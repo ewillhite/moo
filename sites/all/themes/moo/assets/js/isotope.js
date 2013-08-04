ea9731b71812ef28ed8ecb4b97298d1464584428
Evan Willhite <ewillhite@centresource.com>
2013-08-04 14:52:50 -0500

    isotope filtering tweak10

--- sites/all/themes/moo/assets/js/isotope.js
+++ sites/all/themes/moo/assets/js/isotope.js
@@ -14,23 +14,9 @@ $(function() {
     var selector = $(this).attr('data-filter');
     $('.filter a').removeClass('active');
     $(this).addClass('active');
-    $container.isotope({ filter: selector });
-    $.autopager({
-      load: function() {
-        $('div#views_infinite_scroll-ajax-loader').remove();
-        Drupal.attachBehaviors(this);
-        $new_elements = $(".isotope-element:not(.isotope-item)");
-        $(content_selector).imagesLoaded(function(){
-          $(content_selector).isotope('appended', $new_elements, function() {
-            $('.isotope-item').hover(function() {
-              $(this).children('.views-field-nothing').fadeIn('fast');
-            }, function() {
-              $(this).children('.views-field-nothing').fadeOut('fast');
-            });
-          });
-        });
-      }
-    }, 'load');
+    $.autopager('load', function() {
+      $container.isotope({ filter: selector });
+    });
     return false;
   });
 
